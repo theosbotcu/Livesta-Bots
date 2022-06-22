045f3ef3ca213c7d9f22818637d9c13dcfc8b793
Lanceas <68274070+Lanceasfx@users.noreply.github.com>
2022-06-22 20:01:26 +0300

    Update Adoncia.js

--- Adoncia.js
+++ Adoncia.js
@@ -1,5 +1,5 @@
 Adoncia adlı arkadaşın bir gram bile bot yapmışlığı vesayre bilgisi yok ordan burdan alma botları ben yazdım diye geziniyor dark paradise botcusu owsla'nın 
-paylaşılmış botlarını editleyip hatasız şekilde kullanıyor. Güncel botlarında bazı komutları yapabilirim diyordu 3 gün oldu hayla yapıcak sunucuya botcu olarak çağırdık 
-kafasına göre günde 4 ban atıyor kişisel, botcu diye geziniyor birde yazık canzade ile olan düşmanlığında onun botlarını kullanıp devam etmesine rağmen nese daha fazla
+paylaşılmış botlarını editleyip hatasız şekilde kullanıyor. Güncel botlarında bazı komutları yapabilirim diyordu 3 gün oldu hâlâ yapıcak sunucuya botcu olarak çağırdık 
+kafasına göre günde 4 ban atıyor kişisel, botcu diye geziniyor birde yazık canzade ile olan düşmanlığında onun botlarını kullanıp devam etmesine rağmen neyse daha fazla
 anlatmıcam burada livesta adlı proje oluşturdum zamanında livesta adlı sunucu açmıştı orada kullandığı botları paylaşıcam botların sahibi.
 ( Owsla , Ozi ve komutlarda Canzadeye ) aittir.
